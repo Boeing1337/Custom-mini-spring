3dab963443b18cf6c17b44ed5d195ffd4d5b99ed
artem.s <empty>
2022-11-10 12:36:39 +0300

    init

---
+++ src/main/java/WordApplication.java
@@ -0,0 +1,5 @@
+public class WordApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/WordApplicationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordApplicationTest {
+
+    @Test
+    void test(){
+        Assertions.assertDoesNotThrow(()->WordApplication.main(null));
+    }
+
+}
