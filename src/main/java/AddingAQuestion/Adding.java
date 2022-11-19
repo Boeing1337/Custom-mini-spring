7655f7f157a04b4b7230e2c5354d2af0e6715886
artem.s <empty>
2022-11-19 12:50:10 +0300

    Done

--- src/main/java/AddingAQuestion/Adding.java
+++ src/main/java/AddingAQuestion/Adding.java
@@ -3,4 +3,18 @@ package AddingAQuestion;
 public class Adding {
 
 
+    for(
+    int i = 0;
+    i<bukvi.length;i++)
+
+    {
+        String[] a = (bukvi[i].split("/"));
+        String[] b = new String[a.length - a.length / 2 - 1];
+        for (int j = 1, k = 0; j < a.length; j += 2, k++) {
+            b[k] = a[j];
+        }
+        A.add(b);
+    }
+
+
 }
