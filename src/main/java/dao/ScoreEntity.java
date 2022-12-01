4b373c4ae39f3bc2ed69ab95220d71821dc0035c
Nikolya <gaara-1992@bk.ru>
2022-12-01 23:54:38 +0300

    WG-30

--- src/main/java/dao/ScoreEntity.java
+++ src/main/java/dao/ScoreEntity.java
@@ -1,15 +1,18 @@
 package dao;
 
+import java.util.Objects;
+
 public class ScoreEntity {
     private final String login;
-    private final int win;
-
-    private final int loss;
-
-    private final double winRate;
+    private int win;
+    private int loss;
+    private double winRate;
 
 
     public ScoreEntity(String login, int win, int loss, double winRate) {
+        if(login == null) {
+            throw new NullPointerException("Нельзя задать нулевой логин");
+        }
         this.login = login;
         this.win = win;
         this.loss = loss;
@@ -24,8 +27,37 @@ public class ScoreEntity {
         return win;
     }
 
-    public int getLoss() {return loss;}
+    public void setWin(int win) {
+        this.win = win;
+    }
 
-    public double getWinRate() {return winRate;}
+    public int getLoss() {
+        return loss;
+    }
+
+    public void setLoss(int loss) {
+        this.loss = loss;
+    }
+
+    public double getWinRate() {
+        return winRate;
+    }
+
+    public void setWinRate(double winRate) {
+        this.winRate = winRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScoreEntity that = (ScoreEntity) o;
+        return login.equals(that.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
+    }
 }
 
