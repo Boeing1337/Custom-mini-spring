665b1d8600b23ce3e48bbe004bace2b3855995b3
ArtiWell <114738251+ArtiWell@users.noreply.github.com>
2022-12-15 15:43:47 +0300

    bug: WG-102 работа с очками налажена (#48)
    Co-authored-by: artem.s <empty>

--- src/main/java/tech/wg/servise/LadderScoreService.java
+++ src/main/java/tech/wg/servise/LadderScoreService.java
@@ -37,6 +37,7 @@ public class LadderScoreService {
         while (grammar.readInt() != 0) {
             grammar.write("Введена не верная команда");
         }
+        grammar.readLine();
     }
 
 }
