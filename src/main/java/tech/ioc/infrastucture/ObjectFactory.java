79576ef8498993a9e70d9fbe799b41793101aade
artem.s <empty>
2022-12-15 10:55:11 +0300

    feature: WG-95 IoC добавил сообщение об ошибке, если не найден конструктор по умолчанию.

--- src/main/java/tech/ioc/infrastucture/ObjectFactory.java
+++ src/main/java/tech/ioc/infrastucture/ObjectFactory.java
@@ -5,8 +5,12 @@ import tech.ioc.configurator.interfaces.ObjectConfigurator;
 import tech.ioc.configurator.interfaces.ProxyObjectConfigurator;
 import tech.ioc.dto.BeanContainer;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Создает объекты и прогоняет их через все {@link ObjectConfigurator} и {@link ProxyObjectConfigurator}
@@ -31,13 +35,26 @@ public class ObjectFactory {
 
     @SneakyThrows
     public void createBean(BeanContainer beanContainer) {
-        Object t = beanContainer.getImplClass().getDeclaredConstructor().newInstance();
+        Object t = getNoArgConstructorOrThrow(beanContainer).newInstance();
         beanContainer.initBean(t);
         for (ObjectConfigurator configurator : simpleObjectConfigurators) {
             configurator.configure(t, context);
         }
         for (ProxyObjectConfigurator configurator : proxyObjectConfigurators) {
             configurator.configure(beanContainer, context);
+        }
+    }
+
+    private Constructor<?> getNoArgConstructorOrThrow(BeanContainer beanContainer) {
+        return Arrays.stream(beanContainer.getImplClass().getConstructors())
+                .filter(constructor -> constructor.getParameterCount() == 0)
+                .findFirst()
+                .orElseThrow(() -> new NoArgConstructorMissingException(beanContainer.getImplClass()));
+    }
+
+    private static class NoArgConstructorMissingException extends RuntimeException {
+        public NoArgConstructorMissingException(Class<?> object) {
+            super(format("Нет конструктора без аргументов для объекта %s! ", object.getName()));
         }
     }
 }
