04e6c19fb53b83b7d13385dca5802c1942d63492
TRRIT <106248868+TRRIT@users.noreply.github.com>
2022-12-16 18:36:25 +0300

    feature: WG-108 улучшил тесты (#59)
    Co-authored-by: TRRIT <sasha-timofeeva-1994@mail.ru>

--- src/main/java/tech/wg/servise/login/LoginResolver.java
+++ src/main/java/tech/wg/servise/login/LoginResolver.java
@@ -2,10 +2,13 @@ package tech.wg.servise.login;
 
 import tech.ioc.annotations.Component;
 import tech.ioc.annotations.InjectObject;
+import tech.ioc.annotations.InjectProperty;
 import tech.wg.tools.Grammar;
 
 import java.util.List;
 import java.util.Optional;
+
+import static java.lang.String.format;
 
 @Component
 public class LoginResolver {
@@ -16,8 +19,18 @@ public class LoginResolver {
     @InjectObject
     private List<LoginChecker> checkers;
 
+    @InjectProperty
+    private int minLoginLength;
+
+    @InjectProperty
+    private int maxLoginLength;
+
     public Optional<String> resolve() {
-        grammar.println("В логине допустимы только буквы, цифры и знак _ и длинной от 1 до 12 знаков");
+        grammar.println(format(
+                "В логине допустимы только буквы, цифры и знак _ и длинной от %s до %s знаков"
+                , minLoginLength
+                , maxLoginLength)
+        );
         String userInput = grammar.nextLine();
         StringBuilder error = new StringBuilder();
         checkers.forEach(checker -> {
